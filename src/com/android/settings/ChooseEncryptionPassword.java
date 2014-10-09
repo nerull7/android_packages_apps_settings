d08e319f4ea5da7216a62cb8f6a175033628acd9
Przemek Grondek <github@nerull7.info>
2014-10-09 13:59:06 +0200

    Increased encryption password length

--- src/com/android/settings/ChooseEncryptionPassword.java
+++ src/com/android/settings/ChooseEncryptionPassword.java
@@ -92,7 +92,7 @@ public class ChooseEncryptionPassword extends PreferenceActivity {
         private static final String KEY_UI_STAGE = "ui_stage";
         private TextView mPasswordEntry;
         private int mPasswordMinLength = 4;
-        private int mPasswordMaxLength = 16;
+        private int mPasswordMaxLength = 128;
         private int mPasswordMinLetters = 0;
         private int mPasswordMinUpperCase = 0;
         private int mPasswordMinLowerCase = 0;
